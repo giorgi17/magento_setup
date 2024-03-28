c75266625398a795fc88980db9193b021812b86a
giorgi17 <giorgi199817@gmail.com>
2024-03-28 16:54:52 +0400

    Custom theme based on Luma theme

---
+++ src/app/design/frontend/Devall/khachidze/registration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Devall/khachidze',
+    __DIR__,
+);
